f827619ee3b8bc957f4030868d4869cd7b6e158c
Nityakalyani07 <158753924+Nityakalyani07@users.noreply.github.com>
2026-04-10 13:58:23 +0530

    Update demo.py

---
+++ demo.py
@@ -0,0 +1,14 @@
+import random
+
+secret = random.randint(1, 20)
+print("I'm thinking of a number between 1 and 20.")
+
+while True:
+    guess = int(input("Guess the number: "))
+    if guess == secret:
+        print("Correct! You guessed it.")
+        break
+    elif guess < secret:
+        print("Too low!")
+    else:
+        print("Too high!")
